6df1f936dbbccdc3eee355f95dca7a9fb04f99b3
Moy <fabmoy1866@gmail.com>
2020-06-30 20:48:50 -0500

    Agregada Funcionalidad Base de datos al panel add

--- src/com/company/AddProductoPanel.java
+++ src/com/company/AddProductoPanel.java
@@ -1,8 +1,11 @@
 package com.company;
 
 import net.miginfocom.swing.MigLayout;
+import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 
 import javax.swing.*;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class AddProductoPanel extends JPanel {
 
@@ -12,40 +15,45 @@ public class AddProductoPanel extends JPanel {
     private MigLayout layout = new MigLayout(
             "fillx,filly","[][shrink,grow,fill]","[][][][][][]"
     );
+    private JPanel thisComp;
     public AddProductoPanel(SwingComponents allC, MainData allD){
+        thisComp=this;
         allComponents=allC;
         allData=allD;
         setLayout(layout);
         layoutConfig ();
         configEvents ();
+        dataBaseInitialConfig ();
+    }
+    private void dataBaseInitialConfig (){
+        try {
+            ArrayList<String> nombreSecciones = new ArrayList<>();
+            ResultSet resultadoConsulta = allData.getMainStatementDB().executeQuery("SELECT NombreSeccion FROM seccion");
+
+            while (resultadoConsulta.next()){
+                nombreSecciones.add(resultadoConsulta.getString(1));
+            }
+            allComponents.getSeccionAddProductCombobox().setModel(new DefaultComboBoxModel(nombreSecciones.toArray()));
+            AutoCompleteDecorator.decorate(allComponents.getSeccionAddProductCombobox());
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(thisComp,"ERROR EN LA BASE DE DATOS: "+e.getMessage());
+        }
     }
     private void layoutConfig(){
         add (allComponents.getAddProductLogo (),"span 2, align center, wrap");
-
         add(allComponents.getNombreAddProductLabel (),"align left");
         add (allComponents.getNombreAddProductTextField (),"wrap");
-
         add (allComponents.getCodeInternoLabel (),"align left");
         add (allComponents.getCodigointernoTextField (),"wrap");
-
         add (allComponents.getCategory ());
         add (allComponents.getSeccionAddProductCombobox (),"wrap");
-
         add (allComponents.getPrecProveLabel ());
         add (allComponents.getPriceProveedorTextField (),"wrap");
-
-
         add (allComponents.getPriceUnit ());
         add (allComponents.getPrecioVentaAddProductTextField (),"wrap");
-
         add (allComponents.getDescripcionLabel ());
         add (allComponents.getDescripcionTextField (),"wrap");
-
         add (allComponents.getAceptarAddProductButton (),"span 2, align center");
-
-
-
-
     }
     private void configEvents(){
 
--- src/com/company/MainData.java
+++ src/com/company/MainData.java
@@ -1,6 +1,29 @@
 package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 //This class will contain all the Data-structure and variables that will be used by the GUI
 public class MainData {
+    private Statement mainStatementDB;
+    public MainData (){
+        try {
+            Connection conectionDataB= DriverManager.getConnection("jdbc:mysql://localhost:3306/upndowndatabase","root","");
+            mainStatementDB = conectionDataB.createStatement();
+        }catch (Exception e){
+            System.out.println("ERROR AL CONECTARSE A LA BASE DE DATOS: "+e.getMessage());
+        }
+
+    }
+
+    public Statement getMainStatementDB() {
+        return mainStatementDB;
+    }
+
+    public void setMainStatementDB(Statement mainStatementDB) {
+        this.mainStatementDB = mainStatementDB;
+    }
 
     private boolean canClose=true; //VERIFFY IF THE USER CAN CLOSE THE MAIN WINDOW
 
